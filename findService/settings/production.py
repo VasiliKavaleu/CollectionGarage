b29a0f905309dce56997e6318eab1ed15b7e711b
VasiliKavaleu <vasiliy.kavaleu@gmail.com>
2020-06-17 23:23:07 +0300

    update host in settings

--- findService/settings/production.py
+++ findService/settings/production.py
@@ -39,7 +39,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['findcarapp.herokuapp.com']
+ALLOWED_HOSTS = ['collectiongarage.herokuapp.com']
 
 
 # Application definition
